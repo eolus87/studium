c67573f021d1438bfc6f1cd6da4107dacc7dc001
eolus87 <niguvaz@gmail.com>
2023-10-02 22:45:18 +0100

    Included the load and fit nonlinear into the script. Removed the functions from it.

--- transfer_learning/auxiliary_functions.py
+++ transfer_learning/auxiliary_functions.py
@@ -10,14 +10,10 @@ from keras import layers
 from customcallback import CustomCallback
 
 
-def trigonometric(x):
-    y = np.sin(x)
-    return y
 
 
-def trigonometric_shifted(x):
-    y = 2 + np.sin(x - 1)
-    return y
+
+
 
 
 def compile_and_fit_nonlinear(picture_name, neurons, epochs, lr, bs, x_train, y_train, x_test, y_test, minval, maxval):
@@ -37,6 +33,37 @@ def compile_and_fit_nonlinear(picture_name, neurons, epochs, lr, bs, x_train, y_
                                kernel_initializer=initializer,
                                bias_initializer=tf.keras.initializers.Ones()))
     model.add(layers.Dense(1))
+
+    # Compiling the model
+    model.compile(loss=tf.keras.losses.MeanSquaredError(),
+                  optimizer=keras.optimizers.Adam(
+                      learning_rate=lr
+                  ),
+                  metrics=['mean_squared_error', 'mean_absolute_error'])
+
+    # Fitting the model
+    history = model.fit(x_train, y_train,
+                        batch_size=bs,
+                        epochs=epochs,
+                        shuffle=True,
+                        validation_data=(x_test, y_test),
+                        verbose=0,
+                        callbacks=[CustomCallback(picture_name, 100, model, x_train, x_test, y_test)]
+                        )
+
+    model.summary()
+    for i in range(len(model.layers)):
+        print(f"Layer: {i}")
+        print(model.layers[i].weights[0].numpy())
+        print(model.layers[i].bias.numpy())
+    return model, history
+
+
+def load_and_fit_nonlinear(picture_name, model_name, epochs, lr, bs, x_train, y_train, x_test, y_test):
+    tf.keras.backend.clear_session()
+
+    # Initialization of the model
+    model = keras.models.load_model(f"models\\{model_name}")
 
     # Compiling the model
     model.compile(loss=tf.keras.losses.MeanSquaredError(),
